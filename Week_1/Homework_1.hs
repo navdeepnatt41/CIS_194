1cb9ece2beb3343a1a022bb06bc142948ab3e2ff
navdeepnatt41 <nattnavdeep41@gmail.com>
2026-01-02 00:57:01 -0600

    Fixed hanoi function in homework 1

--- Week_1/Homework_1.hs
+++ Week_1/Homework_1.hs
@@ -25,5 +25,4 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 a b c = []
 hanoi 1 a b c = [(a, c)]
-hanoi 2 a b c = [(a, b),(a, c), (b, c)]
-hanoi n a b c = hanoi (n-1) a b c ++ [(a, b)] ++ hanoi (n-1) c a b
+hanoi n a b c = hanoi (n-1) a c b ++ [(a, c)] ++ hanoi (n-1) b a c
