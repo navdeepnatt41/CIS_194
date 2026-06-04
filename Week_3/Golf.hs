67dee9740eef22283974890323e9f4e7666dbe7c
Navdeep Natt <nattnavdeep41@gmail.com>
2026-06-03 22:06:27 -0500

    Reduced code length a little bit after thinking a lot. I figure that if Prelude had transpose, I could write a cleaner implementation for histogram - however, I'm not really interested on exploring that territory. Onto week 4!

--- Week_3/Golf.hs
+++ Week_3/Golf.hs
@@ -1,25 +1,15 @@
 module Golf where
 
 -- skips
+-- Fundamentally, this function is filtering elements based on index
 
-skipHelper :: Int -> Int -> Int -> [a] -> [a]
-skipHelper _ _ _ [] = []
-skipHelper start cur modLimit l
-  | cur == modLimit  = head l : skipHelper start start modLimit (tail l)
-  | otherwise        = skipHelper start (cur + 1) modLimit (tail l)
-
-skipHelper2 :: Int -> [a] -> [Int]
-skipHelper2 _ [] = []
-skipHelper2 cur l = cur : skipHelper2 (cur+1) (tail l)
-
+skipHelper :: Int -> [a] -> [a]
+skipHelper n l = map fst $ filter (\(_, i) -> i `mod` n == 0) $ zip l [1..(length l)]
 
 skips :: [a] -> [[a]]
-skips [] = []
-skips [e] = [[e]]
-skips l = map (\e -> skipHelper 1 1 e l) (skipHelper2 1 l)
+skips l = map (`skipHelper` l) [1..length l]
 
 -- localMaxima
-
 localMaxima :: [Integer] -> [Integer]
 localMaxima [] = []
 localMaxima [x, y] = []
@@ -28,14 +18,6 @@ localMaxima (x:y:z:xs)
   | otherwise            = localMaxima (y:z:xs)
 
 -- histogram
-
-{- 
-	Step 1: Count out the occurrences
-	Step 2: Find the maximum count - that will be how many lines there are. For each line, 
-	you print the respective stars for each number in that line.
-	Step 2: Print the last lines
--}
-
 countOccurrences :: Integer -> [Integer] -> (Integer, Int)
 countOccurrences num nums = (num, length $ filter (== num) nums)
 
@@ -49,10 +31,7 @@ maxHelper :: [Integer] -> Int
 maxHelper = maxOcurrence . allOccurrences
 
 line :: [(Integer, Int)] -> Int -> String
-line [] _ = "\n"
-line ((a, b):xs) n
-  | b >= n      = '*' : line xs n
-  | otherwise           = ' ' : line xs n
+line l n = map (\(_, i) -> if i >= n then '*' else ' ') l ++ ['\n']
 
 histogramSetup :: [Integer] -> String
 histogramSetup m = concatMap (line (allOccurrences m)) [(maxHelper m), (maxHelper m - 1)..1]
